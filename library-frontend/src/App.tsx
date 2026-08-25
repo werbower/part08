4367665515e650b40d4e3df4d3041020a179dc86
Viacheslav P <werbower@gmail.com>
2026-08-25 21:27:49 +0300

    exercise 8.28-8.29

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,12 +1,12 @@
 
-import Authors from './components/authors/authors.component'
+import Authors, { type AuthorData } from './components/authors/authors.component'
 import Books, { type BookData } from './components/books/books.component'
 import NewBook from './components/new-book/new-book.component'
 import { setLogin, useAppStore } from './services/app.service'
 import { Login } from './components/login/login.component'
 import { BooksRecommend } from './components/books-recommend/books-recommend.component'
 import { useApolloClient, useSubscription } from '@apollo/client/react'
-import { subsBookAdded } from './services/apollo.service'
+import { queryAllAuthors, subsBookAdded } from './services/apollo.service'
 
 
 
@@ -15,19 +15,40 @@ const App = () => {
   const setPage = useAppStore(x => x.setPage)
   const token = useAppStore(x => x.token)
   const client = useApolloClient()
-  
+
   useSubscription(subsBookAdded, {
     onData: ({ data }) => {
+      const addedBook = data.data?.bookAdded as any as BookData
+
       client.cache.evict({
         id: 'ROOT_QUERY',
         fieldName: 'allBooks',
       })
-      
+
+      client.cache.updateQuery({ query: queryAllAuthors }, (data) => {
+        const allAuthors = (data?.allAuthors || []) as any as AuthorData[]
+        const addedAuthor = addedBook.author as any as AuthorData
+
+        const found = allAuthors.find(x => x.id === addedAuthor.id)
+        if (found) {
+          const newData = allAuthors.map(a => {
+            if (a.id === addedAuthor.id)
+              a = {...a, bookCount: (a.bookCount||0)+1}
+            return a
+          })
+
+          return { allAuthors: newData } as any
+        }
+
+        return {
+          allAuthors: [...allAuthors,{...addedAuthor, bookCount: 1}]
+        } as any
+      })
+
       window.alert(`added book ${data.data?.bookAdded.title} by ${data.data?.bookAdded.author.name}`)
     }
   })
-
-
+  
   function handleLogout() {
     setLogin(undefined)
     if (['add', 'recommend'].includes(page))
--- library-frontend/src/services/apollo.d.ts
+++
@@ -1,6 +0,0 @@
-import "@apollo/client";
-declare module "@apollo/client" {
-  interface RefetchEvents {
-    booksRefetch: { reason: string } | undefined;
-  }
-}
